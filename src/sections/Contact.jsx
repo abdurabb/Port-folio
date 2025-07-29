cbbb28774ab88c45d8f0a1c5922b3b9c79db49f9
AbdulRubb <abdulrubb@AbdulRubbs-MacBook-Air.local>
2025-07-29 20:29:43 +0400

    contact updated

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -19,11 +19,11 @@ const Contact = () => {
         >
           <div>
             <p className="contact_contactCopy__oCo8c">
-            Feel free to drop me an email for a connection or connect with me through the following platforms!! You can also find me on
+              Feel free to drop me an email for a connection or connect with me through the following platforms!! You can also find me on
               <a href="https://www.linkedin.com/in/abdurabb-cheleri-786b48259/">
                 Linkedin
               </a>
-              
+
               more your speed.
             </p>
           </div>
@@ -49,7 +49,41 @@ const Contact = () => {
                 <span>abdurabb00@gmail.com</span>
               </div>
             </a>
+            <div className="contact_contactEmail__Fr6hH">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 512 512"
+                height="2.4rem"
+                width="2.4rem"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M391.1 330.9c-17.8-17.8-44.5-20.7-65.6-8.6l-27.2 15.5c-4.9 2.8-11 2.1-15-1.8l-55.6-55.6c-3.9-3.9-4.6-10-1.8-15l15.5-27.2c12.1-21.1 9.2-47.8-8.6-65.6L154.2 83.2c-20.2-20.2-53.2-20.2-73.4 0L55 109c-23.1 23.1-28.8 57.6-14.2 85.7 57.1 112.5 153 208.4 265.5 265.5 28.1 14.6 62.6 8.9 85.7-14.2l25.8-25.8c20.2-20.2 20.2-53.2 0-73.4l-26.7-26.7z"></path>
+              </svg>
+              <span>+971 56 819 3783</span>
+            </div>
+
+            <div className="contact_contactEmail__Fr6hH">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 448 512"
+                height="2.4rem"
+                width="2.4rem"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M380.9 97.1C339-3.6 203.5-28.9 113.1 41.5 58.5 84.1 32 149.6 45.8 213.4L1.6 318.5a12 12 0 0 0 15.5 15.5l105.1-44.2C199.7 368 274.5 392.5 337 368c91.1-36.2 130.2-146.1 87.9-236.9zM224 344c-55.6 0-100.8-45.2-100.8-100.8S168.4 142.4 224 142.4 324.8 187.6 324.8 243.2 279.6 344 224 344zm56.5-85.1c-3.2-1.6-18.7-9.2-21.6-10.2-2.9-1.1-5-1.6-7.1 1.6-2.1 3.2-8.2 10.2-10 12.3-1.8 2.1-3.6 2.4-6.8.8-3.2-1.6-13.6-5-25.9-15.9-9.6-8.5-16.1-19-18-22.2-1.9-3.2-.2-5 1.4-6.5 1.4-1.4 3.2-3.6 4.8-5.3 1.6-1.8 2.1-3.2 3.2-5.3 1.1-2.1.5-4-0.2-5.5-0.8-1.6-7.1-17.1-9.7-23.4-2.6-6.3-5.3-5.4-7.1-5.5-1.8-.1-3.9-.1-6-.1s-5.5.8-8.4 4c-2.9 3.2-11 10.8-11 26.4 0 15.6 11.3 30.6 12.9 32.7 1.6 2.1 22.3 33.9 54 47.5 7.6 3.3 13.5 5.3 18.1 6.8 7.6 2.4 14.5 2.1 19.9 1.3 6.1-0.9 18.7-7.6 21.4-14.9 2.6-7.4 2.6-13.7 1.8-14.9-0.7-1.1-2.6-1.8-5.3-3.2z" />
+              </svg>
+              <span>+91 7909199591</span>
+            </div>
+
           </div>
+
+
+
+
           <div className="overlay"></div>
         </div>
       </div>
