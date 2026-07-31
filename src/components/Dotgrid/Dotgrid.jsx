3ffc9c227013b8319891676773c804090d923129
Abdul Rubb Akklyath <abdulrubbakklyath@Abduls-MacBook-Air.local>
2026-07-31 11:49:29 +0400

    use effect upd

--- src/components/Dotgrid/Dotgrid.jsx
+++ src/components/Dotgrid/Dotgrid.jsx
@@ -50,7 +50,7 @@ const Dotgrid = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [startAnimationEvery30Seconds,timer]);
+  }, [timer]);
 
   // Event handler to start the animation from the clicked dot's wrapper
   const handleDotClick = (e) => {
